9d59a9a7aeb61d1b8f9d2f00f1a2368535ab5902
dgagan1 <deshmukhgagan93@gmail.com>
2022-05-03 07:41:21 +0530

    checked in project files

---
+++ pbft/Block.js
@@ -0,0 +1,86 @@
+const SHA256 = require("sha256");
+const ChainUtil = require("./ChainUtil");
+
+class Block{
+    constructor(timestamp, previousHash, currentHash, data, blockProposer, signature, sequenceNo, merkleRoot, bloomFilter){
+        this.timestamp = timestamp;
+        this.currentHash = currentHash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.blockProposer = blockProposer;
+        this.signature = signature;
+        this.sequenceNo = sequenceNo;
+        this.merkleRoot = merkleRoot;
+        this.bloomFilter = bloomFilter;
+    }
+
+
+    toString() {
+        return `Block - 
+            Timestamp   : ${this.timestamp}
+            Last Hash   : ${this.previousHash}
+            Hash        : ${this.currentHash}
+            Data        : ${this.data}
+            proposer    : ${this.blockProposer}
+            Signature   : ${this.signature}
+            Sequence No : ${this.sequenceNo}
+            MerkleRoot  : ${this.merkleRoot}
+            BloomFilter : ${this.bloomFilter}`;
+    }
+
+    static getGenesisBlock(){
+        return new this(
+            `0000000000`,
+            "----",
+            "genesis-hash",
+            [],
+            "AADISH",
+            "ADD44",
+            0
+        );
+    }
+
+    static createBlock(previousBlock, data, wallet, merkleRoot, bloomFilter){
+        var hash;
+        var timestamp = Date.now();
+        var previousHash = previousBlock.hash;
+        hash = Block.hash(timestamp, previousHash, data);
+        var blockProposer = wallet.getPublicKey();
+        var signature = Block.signBlockHash(hash, wallet);
+
+        return new this(
+            timestamp,
+            previousHash,
+            hash,
+            data,
+            blockProposer,
+            signature,
+            previousBlock.sequenceNo + 1,
+            merkleRoot,
+            bloomFilter
+        );  
+    }
+
+    static signBlockHash(hash, wallet){
+        return wallet.sign(hash);
+    }
+
+    static hash(timestamp, previousHash, data){
+        return SHA256(timestamp + previousHash + data);
+    }
+
+    static blockHash(block){
+        const {timestamp, previousHash, data} = block;
+        return Block.hash(timestamp, previousHash, data);
+    }
+
+    static verifyBlock(block){
+        return ChainUtil.verifySignature(block.blockProposer, block.signature, Block.blockHash(block));
+    }
+
+    static verifyProposer(block, blockProposer){
+        return block.blockProposer == blockProposer;
+    }
+}
+
+module.exports = Block;
